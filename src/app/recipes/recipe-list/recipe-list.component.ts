8165b43355a884cc20287253da117f10d8ebc2d2
apischan <apischanskyi@cybervisiontech.com>
2020-08-12 03:34:49 +0300

    added click listeners on recipes

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,21 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from "../recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styles: [
-  ]
+  styles: []
 })
 export class RecipeListComponent implements OnInit {
-
+  @Output() recipeSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is just the test recipe', 'https://upload.wikimedia.org/wikipedia/commons/f/fc/Olivier_Russian_salad_made_to_the_Hermitage_restaurant_recipe.jpg')
+    new Recipe('A Test Recipe', 'This is just the test recipe', 'https://upload.wikimedia.org/wikipedia/commons/f/fc/Olivier_Russian_salad_made_to_the_Hermitage_restaurant_recipe.jpg'),
+    new Recipe('Another Test Recipe', 'This is just the test recipe', 'https://upload.wikimedia.org/wikipedia/commons/f/fc/Olivier_Russian_salad_made_to_the_Hermitage_restaurant_recipe.jpg')
   ];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
 
+  onRecipeSelected(recipeElement: Recipe) {
+    this.recipeSelected.emit(recipeElement);
+  }
 }
