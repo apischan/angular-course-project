2fe433fbbeef100cbd5b1e1c69730fb92c40b0f3
apischan <apischanskyi@cybervisiontech.com>
2020-08-19 02:48:30 +0300

    adding ingredients to shopping list

--- src/app/recipes/recipes.service.ts
+++ src/app/recipes/recipes.service.ts
@@ -1,12 +1,10 @@
-import {EventEmitter} from '@angular/core';
+import {EventEmitter, Injectable} from '@angular/core';
 
 import {Recipe} from "./recipe.model";
 import {Ingredient} from "../shared/ingredient.model";
+import {ShoppingListService} from "../shopping-list/shopping-list.service";
 
-// @ts-ignore
-// @Injectable({
-//   providedIn: 'root'
-// })
+@Injectable()
 export class RecipesService {
   recipeSelected = new EventEmitter<Recipe>();
   private recipes: Recipe[] = [
@@ -28,7 +26,14 @@ export class RecipesService {
       ])
   ];
 
+  constructor(private shoppingListService: ShoppingListService) {
+  }
+
   getRecipes() {
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.shoppingListService.addIngredients(ingredients);
   }
 }
