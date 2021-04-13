c623a508239f48b9e884195e1e255b488b441621
apischanskyi <apischanskyi@cybervisiontech.com>
2021-04-13 20:42:46 +0300

    loading the recipe by id from route

--- src/app/recipes/recipe-detail/recipe-detail.component.ts
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -1,19 +1,29 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {Recipe} from "../recipe.model";
-import {RecipesService} from "../recipes.service";
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from "../recipe.model";
+import { RecipesService } from "../recipes.service";
+import { ActivatedRoute, Params } from "@angular/router";
 
 @Component({
   selector: 'app-recipe-detail',
   templateUrl: './recipe-detail.component.html',
-  styles: [
-  ]
+  styles: []
 })
 export class RecipeDetailComponent implements OnInit {
-  @Input() recipe: Recipe;
+  recipe: Recipe;
+  id: number;
 
-  constructor(private recipesService: RecipesService) { }
+  constructor(private recipesService: RecipesService,
+              private route: ActivatedRoute) {
+  }
 
   ngOnInit(): void {
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.id = +params['id'];
+          this.recipe = this.recipesService.getRecipe(this.id);
+        }
+      );
   }
 
   onAddToShoppingList() {
